c444fcc7e0c097c0cda77d7e9be503b10f40d2dd
RimshakanwalArin <160338555+RimshakanwalArin@users.noreply.github.com>
2024-03-25 14:06:35 +0500

    Add files via upload

---
+++ file create.js
@@ -0,0 +1,7 @@
+console.log("jsconfig.jons ('tsc --init')");
+console.log("packege.jons ('npm init -y')");
+console.log("watch mode ('tsc -w')");
+console.log("typescript to compile javescript ('tsc filename.ts')");
+console.log("print out ('node filename .js')");
+console.log("packege lock.jons('npmmon@2.0.20')");
+console.log("auto mode ('npm nodemon filename.js')");
